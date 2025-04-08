801eca5c9065453391915667be8f6d2732fbe829
unknown <CETECOM@AOAWSC04LC0508.duoc.acad>
2025-04-08 10:06:17 -0400

    paginas hechas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,16 +2,17 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
-  {
     path: 'inicio',
-    loadComponent: () => import('./inicio/inicio.page').then( m => m.InicioPage)
+    loadComponent: () => import('./inicio/inicio.page').then((m) => m.InicioPage),
   },
+  {
+    path: 'footer',
+    loadComponent: () => import('./footer/footer.component').then((m) => m.FooterComponent),
+  },
+  {
+    path: 'header',
+    loadComponent: () => import('./header/header.component').then((m) => m.HeaderComponent),
+  },
+
+
 ];
