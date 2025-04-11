d565ab063f3f3926a53f01a6e8f530ed5201e783
Rodrigo Faúndez <ro.faundezo@duocuc.cl>
2025-04-11 00:32:46 -0400

    login hecho (HTML, CSS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -53,12 +53,12 @@ export const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full', 
   },
   {
     path: '**',
-    redirectTo: 'home',
+    redirectTo: 'login',
   },
 
 
